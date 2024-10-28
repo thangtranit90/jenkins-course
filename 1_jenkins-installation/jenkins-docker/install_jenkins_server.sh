d772ceba8a510bff0521966a08b02b8aadae25ec
thangtranit90 <thangtranit90@gmail.com>
2024-10-28 09:18:23 +0700

    edit install_jenkins_server.sh

--- 1_jenkins-installation/jenkins-docker/install_jenkins_server.sh
+++ 1_jenkins-installation/jenkins-docker/install_jenkins_server.sh
@@ -16,4 +16,4 @@ docker run -p 8080:8080 -p 50000:50000 \
 chmod 666 /var/run/docker.sock
 # show endpoint
 echo 'Jenkins installed'
-echo 'You should now be able to access jenkins at: http://'$(curl -s ifconfig.co)':8080'
+# echo 'You should now be able to access jenkins at: http://'$(curl -s ifconfig.co)':8080'
